108166cd00fd52591c6b2755c5143d7204438787
Cyril Iselin <cyril_iselin@hotmail.com>
2020-09-07 09:11:33 +0200

    29. August (under work) Logic for BankAccount added Unit-Test added

--- BusinessLogic/BankAccount.cs
+++ BusinessLogic/BankAccount.cs
@@ -4,6 +4,35 @@ namespace BusinessLogic
 {
     public class BankAccount
     {
-       
+        private readonly string _customerName;
+        public double Balance { get; private set; }
+
+        public BankAccount(string customerName, double balance)
+        {
+            _customerName = customerName;
+            Balance = balance;
+        }
+
+        public void Debit(double value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("IMPOSSIBLE");
+            }
+
+            Balance = Balance - value;
+        }
+
+        public void Credit(double value)
+        {
+
+        }
+
+
+
+
+
+
+
     }
 }
--- Tests/BankAccountTest.cs
+++ Tests/BankAccountTest.cs
@@ -1,3 +1,4 @@
+using System;
 using BusinessLogic;
 using NUnit.Framework;
 
@@ -14,48 +15,47 @@ namespace NUnitTestProject1
         public void Debit_WithValidAmount_UpdatesBalance()
         {
             // Arrange
-           
+            var account = new BankAccount("Max Muster", 100);
+            double amount = 50;
 
             // Act
-           
+            account.Debit(amount);
 
             // Assert
+            Assert.AreEqual(50, account.Balance);
         }
 
         [Test]
         public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
         {
             // Arrange
+            BankAccount account = new BankAccount("Mr. Bryan Walton", 10);
+            double debitValue = -100;
 
-
-            // Act
-
-
-            // Assert
+            //Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.Debit(debitValue));
         }
 
         [Test]
         public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
         {
             // Arrange
+            BankAccount account = new BankAccount("Mr. Bryan Walton", 10);
+            double debitValue = 100;
 
-
-            // Act
-
-
-            // Assert
+            //Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.Debit(debitValue));
         }
 
         [Test]
         public void Credit_WhenCreditIsLessThanZero_ShouldThrowArgumentOutOfRange()
         {
             // Arrange
+            BankAccount account = new BankAccount("Mr. Bryan Walton", 10);
+            double creditValue = -100;
 
-
-            // Act
-
-
-            // Assert
+            //Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.Credit(creditValue));
         }
     }
 }
